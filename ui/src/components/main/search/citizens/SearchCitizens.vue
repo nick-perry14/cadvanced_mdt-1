7d437ef561e9674259b4bf16c6cec23e0a355b1a
Andrew Isherwood <github@warmlight.co.uk>
2020-03-15 15:56:08 +0000

    Add "Loading" & "No results" messages

--- ui/src/components/main/search/citizens/SearchCitizens.vue
+++ ui/src/components/main/search/citizens/SearchCitizens.vue
@@ -1,30 +1,46 @@
 <template>
     <div>
         <SearchBar v-on:searchChanged="sendSearch($event)" />
-        <SearchResults />
+        <UpdateMessage v-if="isLoading" message="Contacting control..." />
+        <SearchResults v-else />
     </div>
 </template>
 
 <script>
-import SearchBar from './SearchBar.vue';
-import SearchResults from './SearchResults.vue';
-import clientSender from '../../../../mixins/clientSender';
-export default {
-    components: {
-        SearchBar,
-        SearchResults
-    },
-    mixins: [clientSender],
-    methods: {
-        sendSearch(sendObject) {
-            this.sendClientMessage('searchCitizen', sendObject);
+    import UpdateMessage from '../../../reusable/UpdateMessage.vue';
+    import SearchBar from './SearchBar.vue';
+    import SearchResults from './SearchResults.vue';
+    import clientSender from '../../../../mixins/clientSender';
+    export default {
+        components: {
+            SearchBar,
+            SearchResults,
+            UpdateMessage
+        },
+        data: function() {
+            return {
+                isLoading: false
+            };
+        },
+        mixins: [clientSender],
+        methods: {
+            sendSearch(sendObject) {
+                this.isLoading = true;
+                this.sendClientMessage('searchCitizen', sendObject);
+            }
+        },
+        watch: {
+            results: function() {
+                this.isLoading = false;
+            }
+        },
+        computed: {
+            results() {
+                return this.$store.getters.getCitizenSearchResults;
+            }
+        },
+        destroyed: function() {
+            this.$store.commit('setCitizenSearchResults', []);
         }
-    },
-    destroyed: function() {
-        this.$store.commit(
-            'setCitizenSearchResults',
-            []
-        );
-    }
-};
+    };
 </script>
--- ui/src/components/main/search/citizens/SearchResults.vue
+++ ui/src/components/main/search/citizens/SearchResults.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="search-results">
+    <div id="search-results" v-if="results.length > 0">
         <Citizen
             v-for="citizen in results"
             v-on:openCitizenResult="setOpen($event)"
@@ -9,34 +9,37 @@
             :setOpen="setOpen"
         ></Citizen>
     </div>
+    <UpdateMessage v-else message="No results found" />
 </template>
 
 <script>
-import Citizen from '../../../reusable/Citizen/Citizen.vue';
-export default {
-    components: {
-        Citizen
-    },
-    data: function() {
-        return {
-            open: '0'
-        };
-    },
-    methods: {
-        setOpen(open) {
-            this.open = open;
+    import Citizen from '../../../reusable/Citizen/Citizen.vue';
+    import UpdateMessage from '../../../reusable/UpdateMessage.vue';
+    export default {
+        components: {
+            Citizen,
+            UpdateMessage
+        },
+        data: function() {
+            return {
+                open: '0'
+            };
+        },
+        methods: {
+            setOpen(open) {
+                this.open = open;
+            }
+        },
+        computed: {
+            results() {
+                return this.$store.getters.getCitizenSearchResults;
+            }
         }
-    },
-    computed: {
-        results() {
-            return this.$store.getters.getCitizenSearchResults;
-        }
-    }
-};
+    };
 </script>
 
 <style scoped>
-#search_results {
-    overflow-y: auto;
-}
+    #search_results {
+        overflow-y: auto;
+    }
 </style>
